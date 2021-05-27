4fc72094a46b3ab0a5f38963f485f2bfe6a70b17
Nathaniel <nthw.dev@gmail.com>
2021-05-27 17:16:13 -0400

    Day 1 finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from './components/Expenses';
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Car Insurance',
+      amount: 296.96,
+      date: new Date(2021, 4, 27),
+    },
+    { id: 'e2', title: 'Dog food', amount: 96.96, date: new Date(2021, 4, 7) },
+    {
+      id: 'e3',
+      title: 'Trophy Beer',
+      amount: 29.96,
+      date: new Date(2021, 3, 22),
+    },
+    { id: 'e4', title: 'Pizza', amount: 26.96, date: new Date(2021, 2, 21) },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
